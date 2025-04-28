c7de443e5253f9530024b0792c4b6f9dd1b68e35
Tsouli-Abde <93455423+Tsouli-Abde@users.noreply.github.com>
2025-04-28 12:43:10 +0200

    Initial Spring Boot setup

---
+++ src/main/java/com/dauphine/jobnest/JobNestBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dauphine.jobnest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobNestBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobNestBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dauphine/jobnest/JobNestBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dauphine.jobnest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobNestBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
