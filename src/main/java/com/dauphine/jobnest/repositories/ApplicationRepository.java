49ec406f6768019c07d23852df7583a60af5a165
lizyov <liza.khellaf.etudes@gmail.com>
2025-05-17 20:19:02 +0200

    Fixup id type

--- src/main/java/com/dauphine/jobnest/repositories/ApplicationRepository.java
+++ src/main/java/com/dauphine/jobnest/repositories/ApplicationRepository.java
@@ -1,7 +1,10 @@
 package com.dauphine.jobnest.repositories;
 
 import com.dauphine.jobnest.models.Application;
+
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ApplicationRepository extends JpaRepository<Application, Long> {
+public interface ApplicationRepository extends JpaRepository<Application, UUID> {
 }
